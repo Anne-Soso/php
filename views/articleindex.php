ca51d4678c486eb2804deae6bf1a61cd81e064d2
Anne-Sophie Pirlet <annesophie.pirlet@gmail.com>
2015-03-03 09:35:50 +0100

    creation component article

--- views/articleindex.php
+++ views/articleindex.php
@@ -1,14 +1,17 @@
 <div id="articles">
   <?php foreach($data['data'] as $article):?>
-    <h2><?php echo($article->title); ?></h2>
-    <div>
-      <?php echo($article->name)?>
+    <div class="article">
+      <h2 class="article__titre"><?php echo($article->title); ?></h2>
+      <div>
+        <?php echo($article->name)?>
+      </div>
+      <div>
+        <?php echo($article->date)?>
+      </div>
+      <p>
+        <?php echo($article->body)?>
+      </p>
     </div>
-    <div>
-      <?php echo($article->date)?>
-    </div>
-    <p>
-      <?php echo($article->body)?>
-    </p>
+
   <?php endforeach;?>
 </div>
