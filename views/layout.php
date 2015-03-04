f94918c47e35bf8f64dbaa8e14146ac94e9230f7
Anne-Sophie Pirlet <annesophie.pirlet@gmail.com>
2015-03-04 20:03:52 +0100

    changement nav milieu

--- views/layout.php
+++ views/layout.php
@@ -9,30 +9,31 @@
 </head>
 <body>
   <div role="navigation" class="site-nav">
-
-          <a href="#" title="Aller page d'accueil" class="site-nav__element site-nav__element--logo">
-              <img src="img/chavee2.png" class="site-nav__logo" alt="Aller page d'accueil de l'élevage de la Chavée">
-          </a>
-          <a href="#" class="site-nav__element">Blog</a>
-          <a href="#"class="site-nav__element">À propos</a>
-          <a href="#" class="site-nav__element">Chevaux</a>
-          <a href="#" class="site-nav__element">Contact</a>
-          <div class="site-nav__profil">
-            <?php if(!isset($_SESSION['connected'])):?>
-              <a href="?a=collect&e=user">Identifiez-vous</a>
-            <?php else:?>
-              <a href="?a=disconnect&e=user">Déconnectez-vous</a>
-            <?php endif;?>
+          <div class="wrapper">
+            <a href="#" class="site-nav__element">Blog</a>
+            <a href="#"class="site-nav__element">À propos</a>
+            <a href="#" title="Aller page d'accueil" class="site-nav__element site-nav__element--logo">
+                <img src="img/chavee2.png" class="site-nav__logo" alt="Aller page d'accueil de l'élevage de la Chavée">
+            </a>
+            <a href="#" class="site-nav__element">Chevaux</a>
+            <a href="#" class="site-nav__element">Contact</a>
           </div>
   </div>
   <div class="site-video" >
     <img src="img/video.jpg" class="site-video__image"alt="" />
   </div>
   <div class="wrapper">
-    <section class="site-main">
+    <section class="site-main clearfix">
       <h1 class="main-title">Blog de la Chavée</h1>
       <?php include($data['view']);?>
     </section>
+    <div>
+      <?php if(!isset($_SESSION['connected'])):?>
+        <a href="?a=collect&e=user">Identifiez-vous</a>
+      <?php else:?>
+        <a href="?a=disconnect&e=user">Déconnectez-vous</a>
+      <?php endif;?>
+    </div>
   </div>
 </body>
 </html>
